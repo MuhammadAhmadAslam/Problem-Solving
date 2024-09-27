fecde647d1aff37f7b3934b33ac47891c2838379
Muhammad Ahmad <prince1ahmed111@gmail.com>
2024-09-27 20:03:35 +0500

    day1

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,13 @@
-let arr = [1,1,2,4,4,5,5,5,6,7,9]
+let arr = [1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]
 
-  let emptyArr = []  
-for(let i = 0; i < arr.length; i++){
-for(let j = 0; j < arr.length; j++){
- if(arr[i] + arr[j] == 10){
-   emptyArr.push([arr[i] , arr[j]])
- }
+let emptyArr = []
+for (let i = 0; i < arr.length; i++) {
+       for (let j = 0; j < arr.length; j++) {
+              if (arr[i] + arr[j] == 10) {
+                     emptyArr.push([arr[i], arr[j]])
+
+              }
+       }
 }
-}
-console.log(emptyArr);
+
+
