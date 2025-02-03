3b5d30c9f6a4bf073491e31e29872f2fdcd9151e
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2025-02-03 23:01:47 +0500

    new problem solved

--- assets/app.js
+++ assets/app.js
@@ -215,3 +215,19 @@ function main() {
               console.log(total); // Print each result on a new line
        }
 }
+
+
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let i = 0
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) { 
+            i++;
+            nums[i] = nums[j]; 
+        }
+    }
+
+    return i + 1;
+};
